6b7286581821326975ceac4a49fe7bf08bafb95c
jwalkie <jwalkk@umich.edu>
2015-06-19 16:07:48 +0200

    adding more day 4

--- JulianneWalkiewicz-jwalkie/exercises/d4/finding_things.rb
+++ JulianneWalkiewicz-jwalkie/exercises/d4/finding_things.rb
@@ -8,16 +8,28 @@ end
 
 puts index_of('Hey', 'e')
 
-def find_by_name(array_of_hashes, word)
-	array_of_hashes.each do |hash|
-		hash.each do |key, value|
-			if value.include?(word)
-				puts hash
-			end
+
+def find_by_name(array, name) 
+	array.each do |hash|
+		if hash[:name] == name
+			return hash
 		end
 	end
 end
 
+
+
+def filter_by_name(array_of_hashes, name)
+	result = []
+	array_of_hashes.each do |hash|
+		if hash[:name] == name
+			result << hash 
+		end
+	end
+	puts result
+end
+# people = [{name: "Brian"}, {name: "Erica"}, {name: "Brian"}]
+# puts find_by_name(people, "Brian")
 
 
 people = [
@@ -38,4 +50,7 @@ people = [
     :name => "ski"
   }
 ]
-find_by_name(people, "ski")
+puts "---------"
+puts find_by_name(people, "ski")
+puts "---------"
+filter_by_name(people, "ski")
---
+++ JulianneWalkiewicz-jwalkie/exercises/d4/recipes.rb
@@ -0,0 +1,23 @@
+dishes = {
+	'Hash Browns' => ['Potatoes', 'Butter'], 
+	'Coffee' => ['Coffee','Milk', 'Cream', 'Sugar'], 
+	'Salad'=> ['Vegetables', 'Protein', 'Dressing', 'Lettuce']
+}
+
+recipes = {
+	hashbrown: {
+		ingredients: ['Potatoes', 'Butter'],
+		description: 'Fried breakfast potatoes',
+		steps: ['Clean and chop potatoes','Butter a frying pan', 'Add potatoes to pan and wait until golden brown']
+	},
+	coffee: {
+		ingredients: ['Coffee','Milk', 'Cream', 'Sugar'],
+		description: 'Caffeinated beverage made from coffee beans',
+		steps: ['Grind coffee beans', 'Filter with hot water', 'Add milk, cream, or sugar to your liking']
+	},
+	salad: {
+		ingredients: ['Vegetables', 'Protein', 'Dressing', 'Lettuce'],
+		description: 'Vegetables and Protein over a bed lettuce or greens',
+		steps: ['Decide on type of green', 'Add protein', 'Add vegetables']
+	}
+}
